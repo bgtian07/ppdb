37d4c0cbe87e053ff6f3ad985352d3c657c81d3c
bgtian07 <166319738+bgtian07@users.noreply.github.com>
2025-01-20 12:50:34 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="with=device-width, initial-scale=1.0">
+    <title>Pendaftarn Peserta Didik Baru</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.1/css/fontawesome.min.css">
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+</head>
+<body>
+<section class="header">
+    <nav>
+        <a href="index.php"><img src="img/logo2.png" alt="Logo"></a>
+        <div class="nav-links" id="navLinks">
+            <i class="fa fa-times" onclick="hideMenu()"></i>
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="login.php">ADMIN</a></li>
+                <li><a href="daftar.php">DAFTAR</a></li>
+                <li><a href="#">ABOUT US</a></li>
+            </ul>
+        </div>
+        <i class="fa fa-bars" onclick="showMenu()"></i>
+    </nav>
+
+<div class="text-box">
+    <h1>SELAMAT DATANG DI MI NEGERI 1 PEKANBARU</h1>
+    <p>Sekolah inspirasi masa depan<br>Masa depan cerah hiduppun indah</p>
+    <!-- <a href="daftar.php" class="hero-btn">Ayo Daftar</a> -->
+</div>    
+
+</section>
+
+<!----- Course ----->
+
+<section class="course">
+    <h1>PROGRAM UNGGULAN</h1>
+    <p>Bebarapa program yang tersedia di sekolah kami</p>
+
+    <div class="row">
+        <div data-aos="fade-up" data-aos-duration="1000" class="course-col">
+            <h3>TAHFIDZ</h3>
+            <p>Sastra mempelajari bentuk karya cipta atau fiksi yang bersifat imajinatif dan menggunakan bahasa yang indah serta keberadaannya dapat berguna untuk hal-hal lain.</p>
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1500" class="course-col">
+            <h3>TAPAK SUDI</h3>
+            <p>Teknik merupakan suatu prosedur yang rasional agar komponen yang saling berkaitan dapat menjadi satu kesatuan. Selanjutnya, teknik memiliki kegunaan untuk membantu mencapai tujuan yang sudah direncanakan.</p>
+        </div>
+        <div data-aos="fade-up" data-aos-duration="2000" class="course-col">
+            <h3>IZBUL WATHAN</h3>
+            <p>Jurusan kesehatan mempelaari tentang bagaimana kita bisa belajar mengenai prosedur yang berhubungan dengan kesehatan dan juga kedokteran juga</p>
+        </div>
+    </div>
+    <div class="row">
+        <div data-aos="fade-up" data-aos-duration="2000" class="course-col">
+            <h3>PIDATO</h3>
+            <p>Jurusan kesehatan mempelaari tentang bagaimana kita bisa belajar mengenai prosedur yang berhubungan dengan kesehatan dan juga kedokteran juga</p>
+        </div>
+        <div data-aos="fade-up" data-aos-duration="2000" class="course-col">
+            <h3>KSM</h3>
+            <p>Jurusan kesehatan mempelaari tentang bagaimana kita bisa belajar mengenai prosedur yang berhubungan dengan kesehatan dan juga kedokteran juga</p>
+        </div>
+    </div>
+
+
+</section>
+
+<!----- testimonial ----->
+
+<section class="testimonials">
+    <h1>GURU DAN KARYAWAN</h1>
+    <p>Dapat melihat event atau jadwal apa saja yang ada di lapangan futsal</p>
+
+    <div class="row">
+        <div class="testimonials-col">
+            <img src="img/user1.jpg" alt="">
+            <div>
+                <p>Fasilitas yang tersedia di sekolah ini sangat lengkap dan bisa menunjang dalam kegiatan belajar mengajar agar berjalan secara baik</p>
+                <h3>Ria Indriyani</h3>
+            </div>
+        </div>
+        <div class="testimonials-col">
+            <img src="img/user2.jpg" alt="">
+            <div>
+                <p>Pengajar di sekolah ini memiliki kualitas yang sangat bagus dan pembelajaran disampaikan secara menarik</p>
+                <h3>Egi Hidayatullah</h3>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<!----- Facility ----->
+
+<section class="facilities">
+    <h1>FASILITAS KAMI</h1>
+    <p>Beberapa fasilitas yang ada di sekolah kami</p>
+
+    <div class="row">
+        <div class="facilities-col">
+            <img src="img/library.png">
+            <h3>Perpustakaan Masa Depan</h3>
+            <p>Tersedia berbagai macam buku untuk mencari referensi</p>
+        </div>
+        <div class="facilities-col">
+            <img src="img/basketball.png">
+            <h3>Lapangan Luas</h3>
+            <p>Lapangan multifungsi yang bisa digunakan untuk berbagai aktifitas</p>
+        </div>
+        <div class="facilities-col">
+            <img src="img/cafeteria.png">
+            <h3>Kafetaria</h3>
+            <p>Terdapat berbagai macam makanan yang tersedia untuk dinikmati</p>
+        </div>
+    </div>
+
+
+</section>
+
+<!----- campus ----->
+
+<section class="campus">
+    <h1>VISI DAN MISI</h1>
+    <p>kami memiliki visi dan misi  </p>
+
+    <div class="row">
+        <div class="campus-col">
+            <img src="img/london.png">
+            <div class="layer">
+                <h3>ISLAMI</h3>
+            </div>
+        </div>
+        <div class="campus-col">
+            <img src="img/washington.png">
+            <div class="layer">
+                <h3>CERDAS</h3>
+            </div>
+        </div>
+        <div class="campus-col">
+            <img src="img/newyork.png">
+            <div class="layer">
+                <h3>BERAKHLAK BAIK</h3>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<!----- prestasi ---->
+<section class="prestasi" data-aos="fade-up" data-aos-duration="1000">
+    <h1>PRESTASI KAMI</h1>
+    <p>Beberapa prestasi yang telah kami raih</p>
+
+    <div class="row">
+        <div class="prestasi-col">
+            <h3 data-count="150">150</h3>
+            <p>Prestasi Siswa</p>
+        </div>
+        <div class="prestasi-col">
+            <h3 data-count="25">78</h3>
+            <p>Juara Lomba</p>
+        </div>
+        <div class="prestasi-col">
+            <h3 data-count="10">307</h3>
+            <p>Program Unggulan</p>
+        </div>
+    </div>
+</section>
+
+<!-- gallery -->
+<section class="gallery" data-aos="fade-up" data-aos-duration="1000">
+    <h1>Galeri Foto</h1>
+    <p>Beberapa momen terbaik di MI Negeri 1 Pekanbaru</p>
+
+    <div class="gallery-container">
+        <div class="gallery-item" data-aos="zoom-in" data-aos-duration="1000">
+            <img src="img/smk.jpg" alt="Foto 1">
+        </div>
+        <div class="gallery-item" data-aos="zoom-in" data-aos-duration="1200">
+            <img src="img/smk1.jpg" alt="Foto 2">
+        </div>
+        <div class="gallery-item" data-aos="zoom-in" data-aos-duration="1400">
+            <img src="img/smk2.jpg" alt="Foto 3">
+        </div>
+        <div class="gallery-item" data-aos="zoom-in" data-aos-duration="1600">
+            <img src="img/sekolah.jpeg" alt="Foto 4">
+        </div>
+        <div class="gallery-item" data-aos="zoom-in" data-aos-duration="1800">
+            <img src="img/user1.jpg" alt="Foto 5">
+        </div>
+        <div class="gallery-item" data-aos="zoom-in" data-aos-duration="2000">
+            <img src="img/user2.jpg" alt="Foto 6">
+        </div>
+    </div>
+</section>
+
+<!----- ppdb ----->
+
+<section class="cta">
+    <h1>PPDB online MI Muhammadiyah 1 Pekanbaru<br>T.P. 2025-2026</h1>
+    <a href="daftar.php" class="hero-btn">DAFTAR SEKARANG</a>
+</section>
+
+<!-- footer -->
+<section class="footer">
+    <div class="footer-container">
+        <div class="footer-about">
+            <h4>Tentang Kami</h4>
+            <p>Kunjungi kami melalui Instagram @MI_Negeri1Pekanbaru</p>
+            <p>Alamat: Jl. Sumatera No.19, Simpang Empat, Kec. Pekanbaru Kota, Kota Pekanbaru, Riau 28121</p>
+        </div>
+
+        <div class="footer-social">
+            <h4>Ikuti Kami</h4>
+            <div class="social-icons">
+                <a href="https://www.instagram.com/MI_Negeri1Pekanbaru" target="_blank" class="social-icon instagram">
+                    <i class="fa fa-instagram"></i> Instagram
+                </a>
+                <a href="https://www.facebook.com/MI_Negeri1Pekanbaru" target="_blank" class="social-icon facebook">
+                    <i class="fa fa-facebook"></i> Facebook
+                </a>
+                <a href="https://twitter.com/MI_Negeri1Pekanbaru" target="_blank" class="social-icon twitter">
+                    <i class="fa fa-twitter"></i> Twitter
+                </a>
+            </div>
+        </div>
+
+        <div class="footer-map">
+            <h4>Lokasi Kami</h4>
+            <!-- Embed Peta Google -->
+            <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3288.3510820829556!2d101.44658707396538!3d0.5133413636955738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31d5aea71e8bc4c7%3A0xeb134a3e041031e5!2sMI%20Negeri%201%20Pekanbaru!5e1!3m2!1sen!2sid!4v1737350701787!5m2!1sen!2sid" 
+                width="600" 
+                height="550" 
+                style="border:0;" 
+                allowfullscreen="" 
+                loading="lazy" 
+                referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+                
+        </div>
+    </div>
+
+    <div class="footer-bottom">
+        2025 MI Negeri 1 Pekanbaru. All rights reserved.
+    </div>
+</section>
+
+
+
+
+
+
+
+<!-------JavaScript for Toggle Menu-------->
+<script>
+    var navLinks = document.getElementById("navLinks");
+    function showMenu() {
+        navLinks.style.right = "0";
+    }
+    function hideMenu() {
+        navLinks.style.right = "-200px";
+    }
+    
+    // Counter Function with AOS
+    const counters = document.querySelectorAll('[data-count]');
+    const speed = 200; // kecepatan penghitung
+
+    // Fungsi untuk menjalankan counter saat muncul di layar
+    function startCounters() {
+        counters.forEach(counter => {
+            const target = +counter.getAttribute('data-count');
+            const count = +counter.innerText;
+
+            const increment = target / speed;
+
+            if(count < target) {
+                counter.innerText = Math.ceil(count + increment);
+                setTimeout(startCounters, 1);
+            } else {
+                counter.innerText = target;
+            }
+        });
+    }
+
+    // Inisialisasi AOS
+    AOS.init({
+        offset: 120,  // Jarak sebelum animasi muncul
+        once: true,   // Animasi hanya terjadi sekali
+    });
+
+    // Menambahkan event listener pada AOS untuk menjalankan counter saat section muncul
+    AOS.refresh();
+    document.addEventListener('aos:in', startCounters); // Memulai counter saat elemen terdeteksi muncul
+
+</script>
+
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
+
+</body>
+</html>
